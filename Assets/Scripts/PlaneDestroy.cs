79f02347b8ebb236f75a2f14ccda8a843b6e3926
Mezzofloat <skull.wizard.2299@gmail.com>
2023-08-25 11:08:32 -0400

    Added Lives

--- Assets/Scripts/PlaneDestroy.cs
+++ Assets/Scripts/PlaneDestroy.cs
@@ -1,18 +1,35 @@
 using UnityEngine;
 using System;
+using System.Collections.Generic;
 
 public class PlaneDestroy : MonoBehaviour
 {
     [SerializeField] int health;
+    [SerializeField] GameObject heartPrefab;
+    [SerializeField] Transform heartsParent;
 
     public event Action OnPlaneDestroy;
+    List<GameObject> hearts;
+    float x = 0;
+
+    void Awake()
+    {
+        hearts = new List<GameObject>();
+
+        while (hearts.Count < health) {
+            var h = Instantiate(heartPrefab, heartsParent);
+            h.transform.position += new Vector3(x, 0);
+            x -= 75;
+            hearts.Add(h);
+        }
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Rock"))
         {
             health--;
-            Debug.Log("Plane's health is " + health);
+            hearts[^1].GetComponent<Animator>().SetTrigger("Explode");
         }
 
         if (health == 0)
