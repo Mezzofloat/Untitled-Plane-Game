b1b67f21cd8b70d1a81292228983b4106c9454c4
Mezzofloat <skull.wizard.2299@gmail.com>
2023-08-29 09:58:45 -0400

    Added Pearls

--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -5,34 +5,54 @@ using TMPro;
 
 public class PlayerInventory : MonoBehaviour
 {
-    [SerializeField] TMP_Text inventory;
+    [SerializeField] TMP_Text shellsInventory;
+    [SerializeField] TMP_Text pearlsInventory;
+
     [SerializeField] float reachDistance;
-    [SerializeField] GameObject shell;
+    [SerializeField] GameObject shell, pearl;
     
     int shellsInInventory;
+    int pearlsInInventory;
 
     // Start is called before the first frame update
     void OnPickup()
     {
-        for (int i = 0; i < ShellSpawn.shells.Count; i++) {
-            float sqrDistance = (transform.position - ShellSpawn.shells[i].position).sqrMagnitude;
+        for (int i = 0; i < SpawnValuables.shells.Count; i++) {
+            float sqrDistance = (transform.position - SpawnValuables.shells[i].position).sqrMagnitude;
             if (sqrDistance <= reachDistance * reachDistance) {
                 shellsInInventory++;
-                inventory.text = shellsInInventory.ToString();
+                shellsInventory.text = shellsInInventory.ToString();
 
-                Destroy(ShellSpawn.shells[i].gameObject);
-                ShellSpawn.shells.RemoveAt(i);
+                Destroy(SpawnValuables.shells[i].gameObject);
+                SpawnValuables.shells.RemoveAt(i);
             }
-        }         
+        }
+        
+        for (int i = 0; i < SpawnValuables.pearls.Count; i++) {
+            float sqrDistance = (transform.position - SpawnValuables.pearls[i].position).sqrMagnitude;
+            if (sqrDistance <= reachDistance * reachDistance) {
+                pearlsInInventory++;
+                pearlsInventory.text = pearlsInInventory.ToString();
+
+                Destroy(SpawnValuables.pearls[i].gameObject);
+                SpawnValuables.pearls.RemoveAt(i);
+            }
+        }
     }
 
     void OnDrop() {
         if (shellsInInventory > 0) {
             shellsInInventory--;
-            inventory.text = shellsInInventory.ToString();
+            shellsInventory.text = shellsInInventory.ToString();
 
             GameObject newShell = Instantiate(shell, transform.position, transform.rotation);
-            ShellSpawn.shells.Add(newShell.transform);
+            SpawnValuables.shells.Add(newShell.transform);
+        } else if (pearlsInInventory > 0) {
+            pearlsInInventory--;
+            pearlsInventory.text = pearlsInInventory.ToString();
+
+            GameObject newPearl = Instantiate(pearl, transform.position, transform.rotation);
+            SpawnValuables.pearls.Add(newPearl.transform);
         }
     }
 }
---
+++ Assets/Scripts/SpawnValuables.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnValuables : MonoBehaviour
+{
+    [SerializeField] GameObject shell, pearl;
+    [SerializeField] int numShells, numPearls;
+    [SerializeField] CompositeCollider2D tilemap;
+
+    
+    #nullable enable
+    public static List<Transform> shells {get; set;} = new();
+    public static List<Transform> pearls {get; set;} = new();
+
+    // Dictionary<GameObject, (List<Transform>, int)> valuableDictionary = new() {
+    //     { shell, (shells, numShells)},
+    //     { pearl, (pearls, numPearls)}
+    // };
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        tilemap.geometryType = CompositeCollider2D.GeometryType.Polygons;
+
+        shells = new List<Transform>();
+        pearls = new List<Transform>();
+
+        Spawn(shell, numShells);
+        Spawn(pearl, numPearls);
+
+        tilemap.geometryType = CompositeCollider2D.GeometryType.Outlines;
+    }
+
+    void Spawn(GameObject obj, int num) {
+        float x,y;
+
+        List<Transform> objs = new();
+
+        if (obj == pearl) objs = pearls;
+        else if (obj == shell) objs = shells;
+
+        for (int i = 0; i < num; i++) {
+            x = Random.Range(-17f/2, 17f/2);
+            y = Random.Range(-9f/2, 9f/2);
+            Vector3 position = new(x,y);
+
+            if (tilemap.OverlapPoint(position)) {
+                GameObject newValuable = Instantiate(obj, position, Quaternion.identity);
+                objs.Add(newValuable.transform);
+            } else {
+                i--;
+            }
+        }
+    }
+}
