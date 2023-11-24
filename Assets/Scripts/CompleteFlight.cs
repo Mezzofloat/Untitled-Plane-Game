122d1477d0dc3a1bbb28337952a655d6ff142284
Mezzofloat <skull.wizard.2299@gmail.com>
2023-11-24 16:27:46 -0500

    added ending animation to flight

--- Assets/Scripts/CompleteFlight.cs
+++ Assets/Scripts/CompleteFlight.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.ProjectWindowCallback;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
+using Unity.VisualScripting;
+using UnityEngine.InputSystem;
 
 public class CompleteFlight : MonoBehaviour
 {
+    [SerializeField] Transform plane;
+    
     public static int timeInSeconds { private get; set; } = 15;
     public static string destination { private get; set; } = "JanaPeninsula";
 
     void Start() => Invoke("End", 5);
-    void End() => SceneManager.LoadScene(destination);
+
+    void End()
+    {
+        plane.gameObject.GetComponent<PlayerInput>().enabled = false;
+        plane.DORotate(new Vector3(0,0,-30), 1.5f).SetEase(Ease.InQuad);
+        plane.DOMoveX(10, 1.5f).SetEase(Ease.InQuad);
+        plane.DOMoveY(plane.position.y - 7, 1.5f).SetEase(Ease.InQuad).OnComplete(() => SceneManager.LoadScene(destination));
+    }
 }
