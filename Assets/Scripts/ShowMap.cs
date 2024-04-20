ab03153df889479d6cda6327c40e4be1cb921c87
Mezzofloat <skull.wizard.2299@gmail.com>
2024-04-19 23:57:54 -0400

    added animations to the map in splitspit

--- Assets/Scripts/ShowMap.cs
+++ Assets/Scripts/ShowMap.cs
@@ -8,11 +8,55 @@ public class ShowMap : MonoBehaviour
     [SerializeField] GameObject map;
     [SerializeField] Transform plane, player;
 
+    [SerializeField] List<Sprite> animationSprites = new();
+    [SerializeField] float waitTime, delayedWaitTime;
+
+    bool isOpening;
+
     void OnMap() {
-        if ((plane.position - player.position).sqrMagnitude < 3 && map.activeSelf == false) {
+        if ((plane.position - player.position).sqrMagnitude < 3 && !map.activeSelf) {
             map.SetActive(true);
+            OpenClose();
+        } else if (map.activeSelf) {
+            OpenClose();
+        }
+    }
+
+    void OpenClose() {
+        isOpening = !isOpening;
+        StartCoroutine("Animate");
+    }
+
+    IEnumerator Animate() {
+        WaitForSeconds waiting = new WaitForSeconds(waitTime);
+
+        foreach (Transform child in map.transform) {
+            child.gameObject.SetActive(false);
+        }
+
+        if (isOpening) {
+            for (int i = 0; i < animationSprites.Count; i++) {
+                if (i == 2) {
+                    yield return new WaitForSeconds(delayedWaitTime);
+                }
+
+                map.GetComponent<Image>().sprite = animationSprites[i];
+                yield return waiting;
+            }
         } else {
+            for (int i = animationSprites.Count - 1; i > 0; i--) {
+                if (i == 4) {
+                    yield return new WaitForSeconds(delayedWaitTime);
+                }
+
+                map.GetComponent<Image>().sprite = animationSprites[i];
+                yield return waiting;
+            }
             map.SetActive(false);
+        }
+
+        foreach (Transform child in map.transform) {
+            child.gameObject.SetActive(true);
         }
     }
 }
