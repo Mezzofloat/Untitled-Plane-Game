956f481eebe20e94af0b77cd67ef0b89b2768054
Mezzofloat <skull.wizard.2299@gmail.com>
2024-04-19 20:19:50 -0400

    cleaned up trajectory preview code

--- Assets/Scripts/ChangeTrajectoryPreview.cs
+++ Assets/Scripts/ChangeTrajectoryPreview.cs
@@ -4,21 +4,10 @@ using UnityEngine;
 
 public class ChangeTrajectoryPreview : MonoBehaviour
 {
-    //[SerializeField] GameObject janaPeninsula, splitSpit, misIsles, axertAtoll, fregoIslands, rookcastleArchipelago, istempoIsthmus, otoReef;
+    public void OnHover(GameObject caller) {
+        GameObject child = caller.transform.GetChild(1).gameObject;
+        bool enab = !child.activeSelf;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public static void OnHover(GameObject caller) {
-        caller.transform.GetChild(1).gameObject.SetActive(!caller.transform.GetChild(1).gameObject.activeSelf);
+        caller.transform.GetChild(1).gameObject.SetActive(enab);
     }
 }
