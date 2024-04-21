f6070657a111d00299fffdd9fe5609160c833934
Mezzofloat <skull.wizard.2299@gmail.com>
2024-04-21 18:19:20 -0400

    renamed variable to be shorter

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -6,5 +6,5 @@ public class Settings : MonoBehaviour
 {
     public static float Volume;
     public static bool BirdDetectorAsBiggerCamera;
-    public static bool LessTechnicalLanguage;
+    public static bool SimplerLanguage;
 }
