3c97f964c307bee783b8cedc20a41e5aff76c9d6
Mezzofloat <skull.wizard.2299@gmail.com>
2024-04-21 16:34:49 -0400

    added some settings in a script

---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    public static float Volume;
+    public static bool BirdDetectorAsBiggerCamera;
+}
