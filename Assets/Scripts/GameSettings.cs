8f452cc4c6c9f06416905fd0daa6bf9a59795316
Mezzofloat <skull.wizard.2299@gmail.com>
2024-05-05 18:19:19 -0400

    changed settings to be a scriptableobject

---
+++ Assets/Scripts/GameSettings.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class GameSettings : ScriptableObject
+{
+    public float Volume;
+    public bool BirdDetectorAsBiggerCamera;
+    public bool SimplerLanguage;
+}
