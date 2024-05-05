cceed68d38d4d2d37f7ef1b161414296b2282e8c
Mezzofloat <skull.wizard.2299@gmail.com>
2024-05-05 17:29:12 -0400

    fixed bug where player would walk through wall (et al)

--- Assets/Scripts/BoxOnTarget.cs
+++ Assets/Scripts/BoxOnTarget.cs
@@ -1,50 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using UnityEngine.Events;
 
 public class BoxOnTarget : MonoBehaviour
 {
-    [SerializeField] Transform player;
+    [SerializeField] float timeToEnd = 0.65f;
+    [SerializeField] UnityEvent boxEnded;
 
-    #nullable enable
-    Transform? boxOnTarget;
+    Transform boxOnTarget;
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag.Equals("Box")) {
             boxOnTarget = other.transform;
+            StartCoroutine(nameof(EndBox));
         }
     }
 
-    void FixedUpdate()
-    {
-        if (boxOnTarget != null) {
-            if ( (boxOnTarget.position - transform.position).sqrMagnitude < 0.25f ) {
-                boxOnTarget.gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
-                boxOnTarget.gameObject.layer = 0;
-                StartCoroutine(RotateToZero(boxOnTarget));
-
-                player.gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
-                player.position -= (boxOnTarget.position - player.position) * 0.7f;
-                player.gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
-            }
-        }
-    }
-
-    IEnumerator RotateToZero(Transform box) {
-        Quaternion startRotation = box.rotation;
-        float endZRot = 0f;
-        float t = 0;
-
-        while (Mathf.Abs(box.eulerAngles.z) > 0.01)
-        {
-            t = Mathf.Min(1f, t + Time.deltaTime/5 * Mathf.Abs(box.eulerAngles.z));
-            Vector3 angleToRotate = new Vector3(0, 0, Mathf.LerpAngle(box.eulerAngles.z, endZRot, t));
-            box.eulerAngles = angleToRotate;
+    IEnumerator EndBox() {
+        while ( (boxOnTarget.position - transform.position).sqrMagnitude >= 0.25f ) {
             yield return null;
-        } 
-        yield return new WaitForSeconds(0.1f);
-        gameObject.SetActive(false);
+        }
+
+        boxOnTarget.gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+        boxOnTarget.gameObject.layer = 0;
+        boxOnTarget.DORotate(Vector3.zero, timeToEnd);
+        boxOnTarget.DOMove(transform.position, timeToEnd).SetEase(Ease.OutSine);
+        boxEnded.Invoke();
     }
 
     void OnTriggerExit2D(Collider2D other)
