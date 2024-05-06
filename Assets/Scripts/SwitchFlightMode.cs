6177726f5769a8e19123fac74e4205362d324a38
Mezzofloat <skull.wizard.2299@gmail.com>
2024-05-05 22:44:31 -0400

    made transition between flight and breaking

--- Assets/Scripts/SwitchFlightMode.cs
+++ Assets/Scripts/SwitchFlightMode.cs
@@ -2,11 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 public class SwitchFlightMode : MonoBehaviour
 {
+    [SerializeField] Camera mainCam;
+    [SerializeField] float zoomTime;
+
+    void Awake() {
+        
+    }
+
     void OnSwitch()
     {
-        SceneManager.LoadScene(1 - SceneManager.GetActiveScene().buildIndex);
+        SwitchToBreaking();
+    }
+
+    void SwitchToFlight() {
+        mainCam.orthographicSize = 0.01f;
+        mainCam.transform.position = transform.position;
+        mainCam.DOOrthoSize(5, zoomTime).SetEase(Ease.OutSine);
+        mainCam.transform.DOMove(new Vector3(0, 0, -10), zoomTime).OnComplete(() => 
+            SceneManager.LoadScene(1 - SceneManager.GetActiveScene().buildIndex)
+        );   
+    }
+
+    void SwitchToBreaking() {
+        mainCam.DOOrthoSize(0.01f, zoomTime).SetEase(Ease.OutSine);
+        mainCam.transform.DOMove(transform.position, zoomTime).OnComplete(() =>
+            SceneManager.LoadScene(1 - SceneManager.GetActiveScene().buildIndex)
+        );
     }
 }
